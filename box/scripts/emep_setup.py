c9ffb6cbd5196baa9bc3a6389a1f867135332f1c
mifads <david.simpson@met.no>
2020-05-09 15:40:26 +0200

    Adding -vbs etc prefixes

--- box/scripts/emep_setup.py
+++ box/scripts/emep_setup.py
@@ -7,7 +7,7 @@ Usage:
 
   ../scripts/emep_setup.py XXXX  [-d]
 
-  - where XXXX is e.g. EmChem19a or CRI-R5-emep or ...
+  - where XXXX is e.g. EmChem19a-vbs or CRIv2R5Em-H3 or (use emep_setup.py -h to see list)
 
   -d is for debug mode. Waits for user input.
 
@@ -26,30 +26,45 @@ chemscripts='../../chem/scripts'  # ecosx/chem/scripts directory (do.GenChem)
 
 # some useful collections
 # Comments: Aqueous_EmChem16x is still standard
-#  omments: Aqueous_EmChem16z uses Fgas(SO2) and still need checking
-common= ' Aqueous_EmChem16x Aero2017nx ShipNOx FFireInert SeaSalt DustExtended Ash PM_WoodFFuelInert BVOC_SQT_NV' # Most compact but typical EMEP
-dcommon=' Aqueous_EmChem16x Aero2017nx ShipNOx FFireInert SeaSalt Dust BVOC_SQT_NV' # More compact dust
-ocommon=' ShipNOx FFireInert SeaSalt DustExtended Pollen' # Added pollen and extended dust
-fcommon=' Aqueous_EmChem16x Aero2017nx Ash ShipNOx FFireInert SeaSalt DustExtended Pollen BVOC_SQT_NV'
+#           Aqueous_EmChem16z uses Fgas(SO2) and still need checking
+common= ' Aqueous_EmChem16x Aero2017nx ShipNOx PM_FFireInert SeaSalt DustExtended Ash PM_WoodFFuelInert EC_ageing' # Most typical EMEP
+dcommon=' Aqueous_EmChem16x Aero2017nx ShipNOx PM_FFireInert SeaSalt Dust BVOC_SQT_NV' # More compact dust
+ocommon=' ShipNOx PM_FFireInert SeaSalt DustExtended Pollen' # Added pollen and extended dust
+fcommon=' Aqueous_EmChem16x Aero2017nx Ash ShipNOx PM_FFireInert SeaSalt DustExtended Pollen BVOC_SQT_NV'
 ncommon=' Aqueous_EmChem16x Aero2017nx Ash ShipNOx FFireInert16z SeaSalt DustExtended16z Pollen'
 
-# Common emep schemes:
+# Many schemes use common + same BVOC options. We add
+# IsoMT1 adds code for BVOC_SQT_NV, isoprene and apinene as surrogate for all monoterpenes
+# Mainly emissions in these common packages, since chem reactions and species
+# differ betweem EmChems, CRIs and CB6 schemes
+common_IsoMT1  = common + ' BVOC_SQT_NV BVOC_IsoMT1_emis'
+common_IsoMT2  = common + ' BVOC_SQT_NV BVOC_IsoMT2_emis'
+common_IsoMT3  = common + ' BVOC_SQT_NV BVOC_IsoMT3_emis'
+
+# Mix n' match system - can combine various gas and particle packages
+# Common emep combinations given here.
 # (feel free to define your own combinations, but call them something else!)
+# postfixes -vbs.. indicates VBS SOA scheme similar to the "standard" EMEP scheme
+#           -H..   indicates SOA scheme loosely based on Hodzic et al., 2016
+#                   doi:10.5194/acp-16-7917-2016
+#           -M19'  indicates SOA scheme used in McFiggans et al., 2019
+
 cmdx=dict()
-#cmdx['EmChem19']  ='-b EmChem19  -e APINENE_BVOC_emis BVOC_EmChem19 VBS_EmChem16z'+common
-# Nov 2019, use simpler Dust rather than DustExtended
-#cmdx['EmChem19a'] ='-b EmChem19a -e APINENE_BVOC_emis BVOC_EmChem19 VBS_EmChem19'+common
-# order from rv4.33
 
-
-
-#NOT WORKING or SUPPORTED: cmdx['EmChem09']  ='-b EmChem09  -e PM_VBS_acp2012 APINENE_BVOC_emis'+common
-#OLD cmdx['EmChem19']  ='-b EmChem19  -e PM_VBS_EmChem19 BVOC_IsoMT1_emis'+common
-cmdx['EmChem19a'] ='-b EmChem19a -e PM_VBS_EmChem19 BVOC_IsoMT1_emis'+common
-cmdx['EmChem19p'] = cmdx['EmChem19a'] + ' Pollen'
-cmdx['CB6r2']     ='-b CB6r2emep -e PM_Hodzic_CB6'+common # BoxAero # CB6 has rcbio in base
-#cmdx['TestEm'] ='-b EmChem19a -e FFireInert SeaSalt' # TEST
-cmdx['CRI-R5-emep'] ='-b CRI-R5-emep -e '+common
+cmdx['EmChem19a-vbs']  ='-b EmChem19a -e PM_VBS_EmChem19 '+ common_IsoMT1
+cmdx['EmChem19p-vbs']  = cmdx['EmChem19a-vbs'] + ' Pollen'
+cmdx['EmChem19a-vbs3'] ='-b EmChem19a -e BVOC_ExtraMTs PM_VBS_EmChem19 PM_VBS_ExtraMTs'+common_IsoMT3 
+cmdx['EmChem19a-H']    ='-b EmChem19a -e PM_Hodzic_EmChem19'+common_IsoMT1
+#cmdx['TestEm'] ='-b EmChem19a -e FFireInert SeaSalt' # minimal gas/particle
+# CB6s:
+cmdx['CB6r2Em-vbs']   ='-b CB6r2Em -e PM_VBS_CB6r2Em'+common_IsoMT1
+cmdx['CB6r2Em-H']     ='-b CB6r2Em -e PM_Hodzic_CB6' +common_IsoMT1
+# CRIs:
+cmdx['CRIv2R5Em-vbs'] ='-b CRIv2R5Em -e PM_VBS_EmChem19'+common_IsoMT1
+cmdx['CRIv2R5Em-M19'] ='-b CRIv2R5Em -e PM_JPAC_MT3 PM_Hodzic_Aromatics BVOC_XTERP_CRI'+common_IsoMT3
+cmdx['CRIv2R5Em-H1'] ='-b CRIv2R5Em -e PM_Hodzic_EmChem19'+common_IsoMT1
+cmdx['CRIv2R5Em-H2'] ='-b CRIv2R5Em -e PM_Hodzic_EmChem19 PM_Hodzic_BPINENE'+common_IsoMT2
+cmdx['CRIv2R5Em-H3'] ='-b CRIv2R5Em -e PM_Hodzic_EmChem19 PM_Hodzic_BPINENE PM_Hodzic_XTERP BVOC_XTERP_CRI'+common_IsoMT3
 available_emeps= list(cmdx.keys())
 
 Usage += '  '.join(available_emeps)
@@ -60,7 +75,7 @@ if len(sys.argv) < 2:
     parser.print_usage()
     sys.exit(1)
 
-parser.add_argument('chem_mech', help='Base chemical mechanisms (one  needed), e.g. EmChem19',choices=available_emeps)
+parser.add_argument('chem_mech', help='Base chemical mechanisms (one  needed), e.g. EmChem19a',choices=available_emeps)
 parser.add_argument('-d','--dbg',
     help='debug mode. Waits for user input at key steps', action='store_true')
 args = parser.parse_args()
